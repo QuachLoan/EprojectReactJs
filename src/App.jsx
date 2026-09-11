d98724e4ecc65068f1463359d9f2317993c4b0ec
QuachLoan <126428705+QuachLoan@users.noreply.github.com>
2026-09-11 18:21:04 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import MainLayout from './Layouts/MainLayout'
 import Dashboard from './pages/Dashboard/Dasboard'
+import Projects from './pages/Projects/Projects'
 function App() {
 
   return (
       <>
       <Routes>
         <Route element={<MainLayout/>}>
-          <Route path='/' element ={<Dashboard/>}/>
+          <Route index element={<Dashboard/>}/>
+          <Route path='/dashboard' element ={<Dashboard/>}/>
+          <Route path='/Projects' element={<Projects/>}/>
         </Route>
       </Routes>
       </>
